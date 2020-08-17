264bdda8e279d4823a2de0eb73f1b6426e4dafa8
luckyrabbit870930 <67650489+luckyrabbit870930@users.noreply.github.com>
2020-08-17 10:30:14 +0900

    First code upload.
    This is first code uploading.
    HTML/CSS template.

---
+++ Axit.js
@@ -0,0 +1,9 @@
+var body_width = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
+var body_height = window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
+
+var strMsg = "Browser inner window width: " + body_width + ", height: " + body_height + ".";
